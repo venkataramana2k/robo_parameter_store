8f9b7056bd590b0da565f6df95660646a6a7c976
satya <satya.bhimuni123@gmail.com>
2023-12-15 12:21:53 +0530

    project

---
+++ main.tf
@@ -0,0 +1,17 @@
+################# create aws parameter in parameter store terraform ###############
+resource "aws_ssm_parameter" "params" {
+  count = length(var.params)
+  name = var.params[count.index].name
+  type = var.params[count.index].type
+  value = var.params[count.index].value
+}
+
+variable "params" {
+  default = [
+    { name = "roboshop-dev.frontend.catalogue_url", type = "String", value = "http://catalogue-dev.venkata.cloud:8080/"},
+    { name = "roboshop-dev.frontend.cart_url", type = "String", value = "http://cart-dev.venkata.cloud:8080/"},
+    { name = "roboshop-dev.frontend.user_url", type = "String", value = "http://user-dev.venkata.cloud:8080/"},
+    { name = "roboshop-dev.frontend.shipping_url", type = "String", value = "http://shipping-dev.venkata.cloud:8080/"},
+    { name = "roboshop-dev.frontend.payment_url", type = "String", value = "http://payment-dev.venkata.cloud:8080/"},
+  ]
+}
