51f142765f87f102805891471848f245ceb4729d
satya <satya.bhimuni123@gmail.com>
2023-12-16 11:03:05 +0530

    project

--- main.tf
+++ main.tf
@@ -8,10 +8,10 @@ resource "aws_ssm_parameter" "params" {
 
 variable "params" {
   default = [
-    { name = "roboshop.dev.frontend.catalogue_url", type = "String", value = "http://catalogue-dev.venkata.cloud:8080/;"},
-    { name = "roboshop.dev.frontend.cart_url", type = "String", value = "http://cart-dev.venkata.cloud:8080/;"},
-    { name = "roboshop.dev.frontend.user_url", type = "String", value = "http://user-dev.venkata.cloud:8080/;"},
-    { name = "roboshop.dev.frontend.shipping_url", type = "String", value = "http://shipping-dev.venkata.cloud:8080/;"},
-    { name = "roboshop.dev.frontend.payment_url", type = "String", value = "http://payment-dev.venkata.cloud:8080/;"}
+    { name = "roboshop.dev.frontend.catalogue_url", type = "String", value = "http://catalogue-dev.venkata.cloud:8080/"},
+    { name = "roboshop.dev.frontend.cart_url", type = "String", value = "http://cart-dev.venkata.cloud:8080/"},
+    { name = "roboshop.dev.frontend.user_url", type = "String", value = "http://user-dev.venkata.cloud:8080/"},
+    { name = "roboshop.dev.frontend.shipping_url", type = "String", value = "http://shipping-dev.venkata.cloud:8080/"},
+    { name = "roboshop.dev.frontend.payment_url", type = "String", value = "http://payment-dev.venkata.cloud:8080/"}
   ]
 }
